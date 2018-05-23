8e19e94aa217596e9df69a0651bf65a2deb6f2fa
Huy Lê <>
2018-05-23 13:33:03 +1000

    Fix #23: Generate quiz

--- WordFromPic/WordFromPic/Quiz.swift
+++ WordFromPic/WordFromPic/Quiz.swift
@@ -2,17 +2,19 @@ import Foundation
 import UIKit
 
 class Quiz {
-    static let defaultQuiz: Quiz = Quiz(choices: ["cup", "car", "bottle", "people"], image: UIImage(named: "sample")!)
+    static let defaultQuiz: Quiz = Quiz(choices: ["cup", "car", "bottle", "people"], image: UIImage(named: "sample")!, rightAnswer: 0)
     static let numberOfChoices: Int = 4
     static let maxNumberOfQuiz: Int = 4
     static let question: String = "What is this?"
     
     let choices: [String]
+    let rightAnswer: Int
     let image: UIImage
     
-    init(choices: [String], image: UIImage) {
+    init(choices: [String], image: UIImage, rightAnswer: Int) {
         self.choices = choices
         self.image = image
+        self.rightAnswer = rightAnswer
     }
     
     static func generateQuiz() -> [Quiz] {
@@ -23,23 +25,33 @@ class Quiz {
         var listOfNames: [String] = savedObjects.map{$0.name}
         listOfNames.append(contentsOf: defaultQuiz.choices)
         listOfNames = Array(Set(listOfNames))
+        
         let numberOfQuiz = min(maxNumberOfQuiz, savedObjects.count)
         
         var result = [Quiz]()
         for i in 0..<numberOfQuiz {
-            result.append(Quiz(choices: generateChoices(listOfNames, i), image: SavedObject.getImage(imageName: savedObjects[i].imageName)!))
+            let choices = generateChoices(listOfNames, savedObjects[i].name)
+            
+            var rightAnswer: Int = 0
+            for (j, choice) in choices.enumerated() {
+                if (choice == savedObjects[i].name) {
+                    rightAnswer = j
+                }
+            }
+            
+            result.append(Quiz(choices: choices, image: SavedObject.getImage(imageName: savedObjects[i].imageName)!, rightAnswer: rightAnswer))
         }
         
         return result
     }
     
-    static func generateChoices(_ listOfNames: [String], _ rightAnwser: Int) -> [String] {
+    static func generateChoices(_ listOfNames: [String], _ rightAnwser: String) -> [String] {
         let choices = Array(0..<listOfNames.count).shuffled()
         var result = [String]()
         
-        result.append(listOfNames[rightAnwser])
+        result.append(rightAnwser)
         for i in 0..<numberOfChoices {
-            if (choices[i] != rightAnwser) {
+            if (listOfNames[choices[i]] != rightAnwser) {
                 result.append(listOfNames[choices[i]])
             }
             if result.count == numberOfChoices {
