dc1ad42c6e000cf449ee113b60b519a12d8ebf7d
Luo Ho <12497552@student.uts.edu.au>
2018-05-28 20:56:55 +1000

    Fix #3: Resize preview photo

--- WordFromPic/WordFromPic/AppDelegate.swift
+++ WordFromPic/WordFromPic/AppDelegate.swift
@@ -11,19 +11,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
         return self.orientationLock
     }
-
-    struct AppUtility {
-        static func lockOrientation(_ orientation: UIInterfaceOrientationMask) {
-            if let delegate = UIApplication.shared.delegate as? AppDelegate {
-                delegate.orientationLock = orientation
-            }
-        }
-
-        static func lockOrientation(_ orientation: UIInterfaceOrientationMask, andRotateTo rotateOrientation:UIInterfaceOrientation) {
-            self.lockOrientation(orientation)
-            UIDevice.current.setValue(rotateOrientation.rawValue, forKey: "orientation")
-        }
-    }
   
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
--- WordFromPic/WordFromPic/PreviewViewController.swift
+++ WordFromPic/WordFromPic/PreviewViewController.swift
@@ -5,13 +5,19 @@ class PreviewViewController: UIViewController {
     @IBOutlet weak var objectName: UILabel!
     @IBOutlet weak var photo: UIImageView!
     var predictedObject: PredictedObject? = nil
-
+  @IBOutlet weak var soundButton: UIButton!
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         photo.image = predictedObject?.image
         // Do any additional setup after loading the view.
         objectName.text = predictedObject?.name
+        let tap = UITapGestureRecognizer(target: soundAction(soundButton), action: #selector(doubleTapped))
+        tap.numberOfTapsRequired = 2
+        view.addGestureRecognizer(tap)
     }
+  
+    @objc func canRotate() -> Void {}
 
     @IBAction func cancelButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
@@ -29,6 +35,10 @@ class PreviewViewController: UIViewController {
 
     @IBAction func soundAction(_ sender: UIButton) {
         predictedObject?.speak()
+    }
+  
+    @objc func doubleTapped() {
+      // do something here
     }
     
     override var prefersStatusBarHidden : Bool {
